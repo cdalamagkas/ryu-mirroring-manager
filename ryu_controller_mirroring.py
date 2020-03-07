b8a877179e158386f05f11df610de8a5201d94f7
Christos Dalamagkas <cdalamagkas@gmail.com>
2020-03-07 05:49:07 +0200

    Simpler initialisation

--- ryu_controller_mirroring.py
+++ ryu_controller_mirroring.py
@@ -73,142 +73,93 @@ def find_interface(br, ofpt):
     return port
 
 
+def refresh_mirrors(current_bridge, default_src_port=True):
+    # 1. Send the command to get all interfaces of the specific bridge
+    cmd = "ovs-vsctl list-ifaces " + current_bridge
+    stdin, stdout, stderr = client.exec_command(cmd)
+    ifaces = stdout.read().decode('ascii').strip(" ")
+    ifaces_temp = ifaces.split()
+
+    ifaces_src = []
+    ifaces_dst = []
+
+    # 2. Iterate through each interface of the list and check if it is in the exception list. If no, then check if the
+    # iface has been pre-determined as src or dst port. If its role is not pre-determined, then do what the flag
+    # instructs
+    for iface in ifaces_temp:
+        if iface not in exception_list:
+            if iface in source_ports:
+                ifaces_src.append(iface)
+            elif iface in destination_ports:
+                ifaces_dst.append(iface)
+            else:
+                if default_src_port is True:
+                    ifaces_src.append(iface)
+                else:
+                    ifaces_dst.append(iface)
+
+    # 3. Prepare the new port mirroring command
+    cmd = "ovs-vsctl -- set Bridge " + current_bridge + " mirrors=@m "
+
+    # 3.1 Insert the src ports
+    counter_src = 0
+    for iface in ifaces_src:
+        cmd = cmd + " -- --id=@src" + str(counter_src) + " get Port " + iface
+        counter_src = counter_src + 1
+
+    # 3.2 Insert the dst ports
+    counter_dst = 0
+    for iface in ifaces_dst:
+        cmd = cmd + " -- --id=@dst" + str(counter_dst) + " get Port " + iface
+        counter_dst = counter_dst + 1
+
+    # 3.3 Each mirror MUST have a single output port
+    cmd = cmd + " -- --id=@out get Port " + output_ports[mirrors_bridges[current_bridge]]
+
+    # 3.4 Create the mirror object
+    cmd = cmd + " -- --id=@m create Mirror name=" + mirrors_bridges[current_bridge]
+
+    # 3.5 Select the src ports, if any
+    if ifaces_src:
+        cmd = cmd + " select-src-ports="
+    for i in range(0, counter_src - 1):
+        cmd = cmd + "@src" + str(i) + ","
+    cmd = cmd[:-1]  # Delete the last comma
+
+    # 3.6 Select the dst ports, if any
+    cmd = cmd + "select-dst-ports="
+    for i in range(0, counter_dst - 1):
+        cmd = cmd + "@dst" + str(i) + ","
+    cmd = cmd[:-1]  # Delete the last comma
+
+    # 3.7 Last but not least, add the output port
+    cmd = cmd + " output-port=@out"
+
+    # 4. Clear the previous mirror
+    stdin, stdout, stderr = client.exec_command("ovs-vsctl clear Bridge " + current_bridge + " mirrors")
+
+    # 5. Fire up!
+    stdin, stdout, stderr = client.exec_command(cmd)
+
+
 class RyuMirrorManager(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(RyuMirrorManager, self).__init__(*args, **kwargs)
 
-        bridges = list(mirrors_bridges)
-
-        # Clear all mirrors before issuing new ones
-        for current_bridge in bridges:
-            stdin, stdout, stderr = client.exec_command("ovs-vsctl clear Bridge " + current_bridge + " mirrors")
-
         # Issue mirror command for each bridge
         for current_bridge, current_mirror in mirrors_bridges.items():
-            cmd = "ovs-vsctl -- set Bridge " + current_bridge + " mirrors=@m"
-
-            # if the current mirror has source ports, then add them
-            temp_notations_src = temp_notations_dst = []
-
-            if current_mirror in source_ports:
-                i = 0
-                for current_port in source_ports[current_mirror]:
-                    notation = "@src" + str(i)
-                    cmd = cmd + " -- --id=" + notation + " get Port " + str(current_port)
-                    i += 1
-                    temp_notations_src.append(notation)
-
-            # if the current mirror has destination ports, then add them
-            if current_mirror in destination_ports:
-                i = 0
-                for current_port in destination_ports[current_mirror]:
-                    notation = "@dst" + str(i)
-                    cmd = cmd + " -- --id=" + notation + " get Port " + str(current_port)
-                    i += 1
-                    temp_notations_dst.append(notation)
-
-            # Each mirror MUST have a single output port
-            cmd = cmd + " -- --id=@out get Port " + output_ports[current_mirror]
-
-            # Now start creating the mirror
-            cmd = cmd + " -- --id=@m create Mirror name=" + current_mirror
-
-            # Check again if the mirror has source and destination ports. If yes, add them by their notation
-            if current_mirror in source_ports:
-                cmd = cmd + " select-src-port="
-                for item in temp_notations_src:
-                    cmd = cmd + item + ","
-                cmd = cmd[:-1]  # Delete the last comma
-
-            if current_mirror in destination_ports:
-                cmd = cmd + " select-dst-port="
-                for item in temp_notations_dst:
-                    cmd = cmd + item + ","
-                cmd = cmd[:-1]  # Delete the last comma
-
-            # Last but not least, add the output port
-            cmd = cmd + " output-port=@out"
-
-            stdin, stdout, stderr = client.exec_command(cmd)
+            refresh_mirrors(current_bridge)
 
     @set_ev_cls(ofp_event.EventOFPPortStateChange, MAIN_DISPATCHER)
     def update_mirror(self, ev):
-
         if ev.reason == 0:  # this means that a port has been added, therefore, a mirroring session should be updated.
-
             # Send the commands to find bridge and interface
             current_bridge = find_bridge(ev.datapath.id)
+
             iface = find_interface(current_bridge, ev.port_no)
 
-            if iface not in exception_list:
-
+            if iface not in exception_list and current_bridge in mirrors_bridges:
                 default_src_port = True
-
-                # 1. Send the command to get all interfaces of the specific bridge
-                cmd = "ovs-vsctl list-ifaces " + current_bridge
-                stdin, stdout, stderr = client.exec_command(cmd)
-                ifaces = stdout.read().decode('ascii').strip(" ")
-                ifaces_temp = ifaces.split()
-
-                ifaces_src = []
-                ifaces_dst = []
-
-                # 2. Iterate through each interface of the list and check if it is in the exception list. If no, then
-                # check if the iface has been pre-determined as src or dst port. If its role is not pre-determined, then
-                # do what the flag instructs
-                for iface in ifaces_temp:
-                    if iface not in exception_list:
-                        if iface in source_ports:
-                            ifaces_src.append(iface)
-                        elif iface in destination_ports:
-                            ifaces_dst.append(iface)
-                        else:
-                            if default_src_port is True:
-                                ifaces_src.append(iface)
-                            else:
-                                ifaces_dst.append(iface)
-
-                # 3. Prepare the new port mirroring command
-                cmd = "ovs-vsctl -- set Bridge " + current_bridge + " mirrors=@m "
-
-                # 3.1 Insert the src ports
-                counter_src = 0
-                for iface in ifaces_src:
-                    cmd = cmd + " -- --id=@src" + str(counter_src) + " get Port " + iface
-                    counter_src = counter_src + 1
-
-                # 3.2 Insert the dst ports
-                counter_dst = 0
-                for iface in ifaces_dst:
-                    cmd = cmd + " -- --id=@dst" + str(counter_dst) + " get Port " + iface
-                    counter_dst = counter_dst + 1
-
-                # 3.3 Each mirror MUST have a single output port
-                cmd = cmd + " -- --id=@out get Port " + output_ports[mirrors_bridges[current_bridge]]
-
-                # 3.4 Create the mirror object
-                cmd = cmd + " -- --id=@m create Mirror name=" + mirrors_bridges[current_bridge]
-
-                # 3.5 Select the src ports, if any
-                if ifaces_src:
-                    cmd = cmd + " select-src-ports="
-                for i in range(0, counter_src-1):
-                    cmd = cmd + "@src" + str(i) + ","
-                cmd = cmd[:-1]  # Delete the last comma
-
-                # 3.6 Select the dst ports, if any
-                cmd = cmd + "select-dst-ports="
-                for i in range(0, counter_dst-1):
-                    cmd = cmd + "@dst" + str(i) + ","
-                cmd = cmd[:-1]  # Delete the last comma
-
-                # 3.7 Last but not least, add the output port
-                cmd = cmd + " output-port=@out"
-
-                # 4. Clear the previous mirror
-                stdin, stdout, stderr = client.exec_command("ovs-vsctl clear Bridge " + current_bridge + " mirrors")
-
-                # 5. Fire up!
-                stdin, stdout, stderr = client.exec_command(cmd)
+                refresh_mirrors(default_src_port, current_bridge)
