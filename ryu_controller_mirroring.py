f3cf926ac5521c64ac5f5325403ba593b4c9fbf9
Christos Dalamagkas <cdalamagkas@gmail.com>
2020-03-07 03:56:03 +0200

    New version of Ryu mirroring manager

--- ryu_controller_mirroring.py
+++ ryu_controller_mirroring.py
@@ -9,7 +9,7 @@ import paramiko
 # NOTE that in these comments, the OVS terminology is used when referring to port mirroring (Cisco terminology differs)
 
 # Those interfaces are excluded for any port mirroring configuration.
-exception_list = ["eno1", "eno2", "eno3"]
+exception_list = ["eno1", "eno2", "eno3", "eno4", "tap104i0", "tap108i0"]
 
 # The output ports of each mirror are pre-determined here. This dictionary cannot be empty!
 output_ports = {"mgmt-ovs": "tap111i1",
@@ -20,20 +20,20 @@ output_ports = {"mgmt-ovs": "tap111i1",
                 "han-aruba": "tap112i1",
                 "ian-aruba": "tap113i1"}
 
-# The destination ports to initialise for each mirror are determined here. You can left this dict entirely emtpy.
-destination_ports = {
-    "mgmt-ovs": ["tap100i0", "tap102i0", "tap101i0"],
-    "han-ovs": ["tap115i1"],
-    "ian-ovs": ["tap116i1"],
-    "dmz-ovs": ["tap117i1"]
-}
-
-# The destination ports to initialise for each mirror are determined here. You can left this dict entirely emtpy.
+# The source ports (cisco: ingress source) to initialise for each mirror are determined here.
+# You can left this dict entirely emtpy.
 source_ports = {
+    "mgmt-ovs": ["tap100i0", "tap102i0", "veth103i0"],
+    "han-ovs": ["tap100i1", "tap101i0"],
+    "ian-ovs": ["tap100i2", "tap107i0"],
     "mgmt-aruba": ["eno4"],
     "han-aruba": ["ens4f1"],
     "ian-aruba": ["ens4f2"]
 }
+
+# The destination ports (cisco: egress source) to initialise for each mirror are determined here.
+# You can left this dict entirely emtpy.
+destination_ports = {}
 
 # This dictionary maps each mirror configuration to a bridge. BE SURE that the bridges names are correct
 mirrors_bridges = {
@@ -42,16 +42,15 @@ mirrors_bridges = {
     "vmbr2": "ian-ovs",
     "vmbr3": "mgmt-aruba",
     "vmbr5": "han-aruba",
-    "vmbr6": "ian-aruba",
-    "vmbr99": "dmz-ovs"
-}
+    "vmbr6": "ian-aruba"}
 
 # SSH initialisation to execute OVS-related commands. CHANGE the client.connect(..) with the credentials used by the
 # host that runs the OVSDB server.
 key = paramiko.RSAKey.from_private_key_file("/root/.ssh/proxmox-private.key")
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect('example.com', username='root', pkey=key)
+client.connect('ryu.trsc.net', username='root', pkey=key)
+
 
 # This command returns the name of a bridge, given its datapath id.
 def find_bridge(dpid):
@@ -89,6 +88,7 @@ class RyuMirrorManager(app_manager.RyuApp):
         # Issue mirror command for each bridge
         for current_bridge, current_mirror in mirrors_bridges.items():
             cmd = "ovs-vsctl -- set Bridge " + current_bridge + " mirrors=@m"
+
             # if the current mirror has source ports, then add them
             temp_notations_src = temp_notations_dst = []
 
@@ -139,33 +139,76 @@ class RyuMirrorManager(app_manager.RyuApp):
         if ev.reason == 0:  # this means that a port has been added, therefore, a mirroring session should be updated.
 
             # Send the commands to find bridge and interface
-            bridge = find_bridge(ev.datapath.id)
-            iface = find_interface(bridge, ev.port_no)
+            current_bridge = find_bridge(ev.datapath.id)
+            iface = find_interface(current_bridge, ev.port_no)
 
             if iface not in exception_list:
 
-                src_values = source_ports.values()
-                dst_values = destination_ports.values()
-                out_values = output_ports.values()
+                default_src_port = True
 
-                # First check if the new iface is already a pre-determined source, destination or output port.
-                if iface in [x for v in src_values for x in v if type(v) == list] or iface in src_values:
-                    cmd = "ovs-vsctl -- --id=@src0 get Port " + iface + " -- add Mirror " + mirrors_bridges[
-                        bridge] + "select-src-port @src0"
-                    stdin, stdout, stderr = client.exec_command(cmd)
+                # 1. Send the command to get all interfaces of the specific bridge
+                cmd = "ovs-vsctl list-ifaces " + current_bridge
+                stdin, stdout, stderr = client.exec_command(cmd)
+                ifaces = stdout.read().decode('ascii').strip(" ")
+                ifaces_temp = ifaces.split()
 
-                elif iface in [x for v in dst_values for x in v if type(v) == list] or iface in dst_values:
-                    cmd = "ovs-vsctl -- --id=@dst0 get Port " + iface + " -- add Mirror " + mirrors_bridges[
-                        bridge] + "output-port @dst0"
-                    stdin, stdout, stderr = client.exec_command(cmd)
+                ifaces_src = []
+                ifaces_dst = []
 
-                elif iface in [x for v in out_values for x in v if type(v) == list] or iface in out_values:
-                    cmd = "ovs-vsctl -- --id=@out get Port " + iface + " -- add Mirror " + mirrors_bridges[
-                        bridge] + "output-port @out"
-                    stdin, stdout, stderr = client.exec_command(cmd)
+                # 2. Iterate through each interface of the list and check if it is in the exception list. If no, then
+                # check if the iface has been pre-determined as src or dst port. If its role is not pre-determined, then
+                # do what the flag instructs
+                for iface in ifaces_temp:
+                    if iface not in exception_list:
+                        if iface in source_ports:
+                            ifaces_src.append(iface)
+                        elif iface in destination_ports:
+                            ifaces_dst.append(iface)
+                        else:
+                            if default_src_port is True:
+                                ifaces_src.append(iface)
+                            else:
+                                ifaces_dst.append(iface)
 
-                # If not, add the new port as dst-port (you can change this part at your own preference)
-                else:
-                    cmd = "ovs-vsctl -- --id=@dst0 get Port " + iface + " -- add Mirror " + mirrors_bridges[
-                        bridge] + "select-dst-port @dst0"
-                    stdin, stdout, stderr = client.exec_command(cmd)
+                # 3. Prepare the new port mirroring command
+                cmd = "ovs-vsctl -- set Bridge " + current_bridge + " mirrors=@m "
+
+                # 3.1 Insert the src ports
+                counter_src = 0
+                for iface in ifaces_src:
+                    cmd = cmd + " -- --id=@src" + str(counter_src) + " get Port " + iface
+                    counter_src = counter_src + 1
+
+                # 3.2 Insert the dst ports
+                counter_dst = 0
+                for iface in ifaces_dst:
+                    cmd = cmd + " -- --id=@dst" + str(counter_dst) + " get Port " + iface
+                    counter_dst = counter_dst + 1
+
+                # 3.3 Each mirror MUST have a single output port
+                cmd = cmd + " -- --id=@out get Port " + output_ports[mirrors_bridges[current_bridge]]
+
+                # 3.4 Create the mirror object
+                cmd = cmd + " -- --id=@m create Mirror name=" + mirrors_bridges[current_bridge]
+
+                # 3.5 Select the src ports, if any
+                if ifaces_src:
+                    cmd = cmd + " select-src-ports="
+                for i in range(0, counter_src-1):
+                    cmd = cmd + "@src" + str(i) + ","
+                cmd = cmd[:-1]  # Delete the last comma
+
+                # 3.6 Select the dst ports, if any
+                cmd = cmd + "select-dst-ports="
+                for i in range(0, counter_dst-1):
+                    cmd = cmd + "@dst" + str(i) + ","
+                cmd = cmd[:-1]  # Delete the last comma
+
+                # 3.7 Last but not least, add the output port
+                cmd = cmd + " output-port=@out"
+
+                # 4. Clear the previous mirror
+                stdin, stdout, stderr = client.exec_command("ovs-vsctl clear Bridge " + current_bridge + " mirrors")
+
+                # 5. Fire up!
+                stdin, stdout, stderr = client.exec_command(cmd)
